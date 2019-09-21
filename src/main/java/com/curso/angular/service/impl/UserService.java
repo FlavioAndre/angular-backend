37532a0d2a9501300039e85b730b830559937164
Flavio Andre <flavio.andre@datainfo.inf.br>
2019-09-20 21:20:35 -0300

    ajustes na regra de exlusão do user

--- src/main/java/com/curso/angular/service/impl/UserService.java
+++ src/main/java/com/curso/angular/service/impl/UserService.java
@@ -2,16 +2,20 @@ package com.curso.angular.service.impl;
 
 import java.util.Optional;
 
+import javax.validation.constraints.NotNull;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.curso.angular.base.exception.ServiceException;
 import com.curso.angular.base.service.ServiceCrudAbstractImpl;
 import com.curso.angular.dto.UserDetailsDTO;
 import com.curso.angular.entity.User;
 import com.curso.angular.repository.UserRepository;
+import com.curso.angular.service.exceptions.AuthorizationException;
 import com.curso.angular.service.interfaces.UserServiceInterface;
 
 import lombok.AllArgsConstructor;
@@ -36,4 +40,13 @@ public class UserService extends ServiceCrudAbstractImpl<User, Long> implements 
 		return userRepository.findByEmail(email);
 	}
 
+	@Override
+	public void delete(@NotNull Long pk) throws ServiceException {
+		if(pk.equals(1L)) {
+			throw new AuthorizationException("Não é permitido excluir o ID número 1");
+		}
+		super.delete(pk);
+	}
+
+	
 }
