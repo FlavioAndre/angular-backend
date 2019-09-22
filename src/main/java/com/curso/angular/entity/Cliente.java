25aa8767996aeb85563fe974542f5cb9435e8e73
Flavio Andre <flavio.andre@datainfo.inf.br>
2019-09-22 20:39:49 -0300

    ajustes no mapeamento de cliente

--- src/main/java/com/curso/angular/entity/Cliente.java
+++ src/main/java/com/curso/angular/entity/Cliente.java
@@ -1,18 +1,16 @@
 package com.curso.angular.entity;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.Length;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -37,12 +35,39 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer codigoTipoCliente;
 	private String telefone;
+
+	@NotBlank(message = "O logradouro é obrigatório.")
+	@Length(max = 255)
+	@Column(name = "rua", length = 255)
+	private String rua;
 	
-	@OneToMany(mappedBy="cliente", cascade=CascadeType.ALL)
-	private List<Endereco> enderecos = new ArrayList<>();
+	@NotBlank(message = "O número é obrigatório.")
+	@Length(max = 10)
+	@Column(name = "numero", length = 10)
+	private String numero;
+
+	@Length(max = 255)
+	@Column(name = "complemento", length = 255)
+	private String complemento;
 	
-	@JsonIgnore
-	@OneToMany(mappedBy="cliente", cascade=CascadeType.ALL)
-	private List<Documento> documentos = new ArrayList<>();
+	@NotBlank(message = "O bairro é obrigatório.")
+	@Length(max = 255)
+	@Column(name = "bairro", length = 255)
+	private String bairro;
+	
+	@NotBlank(message = "O cep é obrigatório.")
+	@Length(max = 10)
+	@Column(name = "cep", length = 10)
+	private String cep;
+	
+	@NotBlank(message = "A cidade é obrigatório.")
+	@Length(max = 255)
+	@Column(name = "cidade", length = 255)
+	private String cidade;
+	
+	@NotBlank(message = "o estado é obrigatório.")
+	@Length(max = 2)
+	@Column(name = "estado", length = 2)
+	private String estado;
 	
 }
