f53a672604b81da50e31b8cf0b23e5aaf6fca18f
Flavio Andre <flavio.andre@datainfo.inf.br>
2019-09-24 23:17:12 -0300

    ajustes cors

--- src/main/java/com/curso/angular/AngularApplication.java
+++ src/main/java/com/curso/angular/AngularApplication.java
@@ -1,7 +1,15 @@
 package com.curso.angular;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class AngularApplication {
@@ -10,5 +18,18 @@ public class AngularApplication {
 		SpringApplication.run(AngularApplication.class, args);
 	}
 
-
+	 @Bean
+	    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        CorsConfiguration config = new CorsConfiguration();
+	        config.setAllowCredentials(true);
+	        config.setAllowedOrigins(Collections.singletonList("*"));
+	        config.setAllowedMethods(Collections.singletonList("*"));
+	        config.setAllowedHeaders(Collections.singletonList("*"));
+	        source.registerCorsConfiguration("/**", config);
+	        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+	        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	        return bean;
+	    }
+	
 }
